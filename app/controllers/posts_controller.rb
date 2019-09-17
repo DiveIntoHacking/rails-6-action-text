694a4a1b30bf0c93d99068994819578fd6eba79e
Atsushi Ishida <gipcompany@gmail.com>
2019-09-17 12:22:26 +0900

    add content in permitted params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title)
+      params.require(:post).permit(:title, :content)
     end
 end
