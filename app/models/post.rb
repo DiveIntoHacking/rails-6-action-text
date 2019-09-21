6f6935643222cb1f4c987ad7266d3fd87a08ee7e
Atsushi Ishida <gipcompany@gmail.com>
2019-09-22 05:32:09 +0900

    implement attachment count validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,14 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 32 }, presence: true
 
   validate :validate_content_attachment_byte_size
+  validate :validate_content_attachments_count
   validate :validate_content_length
 
   MAX_CONTENT_LENGTH = 50
   ONE_KILOBYTE = 1024
   MEGA_BYTES = 4
   MAX_CONTENT_ATTACHMENT_BYTE_SIZE = MEGA_BYTES * 1_000 * ONE_KILOBYTE
+  MAX_CONTENT_ATTACHMENTS_COUNT = 4
 
   private
 
@@ -24,6 +26,17 @@ class Post < ApplicationRecord
           max_bytes: MAX_CONTENT_ATTACHMENT_BYTE_SIZE
         )
       end
+    end
+  end
+
+  def validate_content_attachments_count
+    if content.body.attachables.grep(ActiveStorage::Blob).count >
+      MAX_CONTENT_ATTACHMENTS_COUNT
+      errors.add(
+        :content,
+        :attachments_count_is_too_big,
+        max_content_attachments_count: MAX_CONTENT_ATTACHMENTS_COUNT
+      )
     end
   end
 
