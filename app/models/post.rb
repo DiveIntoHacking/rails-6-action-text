552498ca5a2c68aee67fa284c60201ab0640d234
Atsushi Ishida <gipcompany@gmail.com>
2019-09-22 00:57:20 +0900

    content length validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,21 @@ class Post < ApplicationRecord
   has_rich_text :content
 
   validates :title, length: { maximum: 32 }, presence: true
+
+  validate :validate_content_length
+
+  MAX_CONTENT_LENGTH = 50
+
+  def validate_content_length
+    length = content.to_plain_text.length
+
+    if length > MAX_CONTENT_LENGTH
+      errors.add(
+        :content,
+        :too_long,
+        max_content_length: MAX_CONTENT_LENGTH,
+        length: length
+      )
+    end
+  end
 end
